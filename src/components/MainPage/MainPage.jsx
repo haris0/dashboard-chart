ba9c5e855d279cd69bb95991c77f491f667e4707
Hari Surrisyad <hari.surrisyad@gmail.com>
2022-01-24 17:39:56 +0700

    set row and col

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import './MainPage.scss';
+import {
+  Col,
+  Row,
+} from 'react-bootstrap';
 
 const MainPage = () => (
-  <div>
-    Main Page
+  <div className="main-page">
+    <Row>
+      <Col md={3}>Satu</Col>
+      <Col md={3}>Satu</Col>
+      <Col md={6}>Satu</Col>
+    </Row>
+    <Row>
+      <Col md={4}>Satu</Col>
+      <Col md={8}>Satu</Col>
+    </Row>
   </div>
 );
 
